0328cc5dac1f360916aed1b8fa0882aec23499e6
Firman Aulia <firmanaulia@Firmans-MacBook-Air.local>
2026-05-29 05:08:09 +0700

    fix(showtime): add json tags to Showtime struct

--- internal/domain/showtime.go
+++ internal/domain/showtime.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Showtime struct {
-	ID          uuid.UUID `db:"id"`
-	MovieTitle  string    `db:"movie_title"`
-	StartTime   time.Time `db:"start_time"`
-	EndTime     time.Time `db:"end_time"`
-	Price       float64   `db:"price"`
-	TotalSeats  int       `db:"total_seats"`
-	BookedSeats int       `db:"booked_seats"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
+	ID          uuid.UUID `db:"id"           json:"id"`
+	MovieTitle  string    `db:"movie_title"  json:"movie_title"`
+	StartTime   time.Time `db:"start_time"   json:"start_time"`
+	EndTime     time.Time `db:"end_time"     json:"end_time"`
+	Price       float64   `db:"price"        json:"price"`
+	TotalSeats  int       `db:"total_seats"  json:"total_seats"`
+	BookedSeats int       `db:"booked_seats" json:"booked_seats"`
+	CreatedAt   time.Time `db:"created_at"   json:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"   json:"updated_at"`
 }
